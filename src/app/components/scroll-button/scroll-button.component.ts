907a4765f4e5ceb5993e0fce502c054169f0278b
Mauricio González <hello@mauro-gonzalez.dev>
2024-10-29 22:01:58 -0300

    Modified scrollTo function

--- src/app/components/scroll-button/scroll-button.component.ts
+++ src/app/components/scroll-button/scroll-button.component.ts
@@ -37,7 +37,7 @@ export class ScrollButtonComponent {
         document.documentElement.scrollTop || document.body.scrollTop;
       if (currentScroll > 0) {
         window.requestAnimationFrame(smoothscroll);
-        window.scrollTo(0, currentScroll - currentScroll / 0.5);
+        window.scrollTo(0, 0);
       }
     })();
   }
