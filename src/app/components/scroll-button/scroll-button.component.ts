2423d4474deb9e6d13237823696939e7af6b8325
Mauricio González <hello@mauro-gonzalez.dev>
2024-10-29 22:13:46 -0300

    Deleted request animation frame for scrollTo

--- src/app/components/scroll-button/scroll-button.component.ts
+++ src/app/components/scroll-button/scroll-button.component.ts
@@ -36,7 +36,6 @@ export class ScrollButtonComponent {
       const currentScroll =
         document.documentElement.scrollTop || document.body.scrollTop;
       if (currentScroll > 0) {
-        window.requestAnimationFrame(smoothscroll);
         window.scrollTo(0, 0);
       }
     })();
