863ff9bfc6a84dde86ce54d785a09510f73a2a22
Mauricio González <hello@mauro-gonzalez.dev>
2024-11-04 15:04:21 -0300

    Added site logo & refactor for theme switcher

--- src/app/services/theme-switcher.service.ts
+++ src/app/services/theme-switcher.service.ts
@@ -11,15 +11,7 @@ export class ThemeSwitcherService {
     if (isPlatformBrowser(this._platformId)) {
       // TODO: El servicio se esta instanciando cada vez que hay un window resize -> moverlo a un componente padre
       console.log('init');
-      if (
-        localStorage.getItem('color-theme') === 'dark' ||
-        (!('color-theme' in localStorage) &&
-          window.matchMedia('(prefers-color-scheme: dark)').matches)
-      ) {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
+
       // Change the icons inside the button based on previous settings
       const themeToggleDarkIcon = document.getElementById(
         'theme-toggle-dark-icon'
@@ -28,14 +20,21 @@ export class ThemeSwitcherService {
         'theme-toggle-light-icon'
       );
 
+      // Logo selector
+      const siteLogo: HTMLImageElement = document.getElementById(
+        'site-logo'
+      ) as HTMLImageElement;
+
       if (
         localStorage.getItem('color-theme') === 'dark' ||
         (!('color-theme' in localStorage) &&
           window.matchMedia('(prefers-color-scheme: dark)').matches)
       ) {
         themeToggleLightIcon!.classList.remove('hidden');
+        this._setLogoStyle(siteLogo, 'dark');
       } else {
         themeToggleDarkIcon!.classList.remove('hidden');
+        this._setLogoStyle(siteLogo, 'light');
       }
 
       const themeToggleBtn = document.getElementById('theme-toggle');
@@ -50,9 +49,11 @@ export class ThemeSwitcherService {
           if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
+            this._setLogoStyle(siteLogo, 'dark');
           } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
+            this._setLogoStyle(siteLogo, 'light');
           }
 
           // If NOT set via local storage previously
@@ -60,12 +61,27 @@ export class ThemeSwitcherService {
           if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
+            this._setLogoStyle(siteLogo, 'light');
           } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
+            this._setLogoStyle(siteLogo, 'dark');
           }
         }
       });
+    }
+  }
+
+  private _setLogoStyle(
+    logoRef: HTMLImageElement,
+    style: 'dark' | 'light'
+  ): void {
+    if (style === 'dark') {
+      logoRef.src = '/logo-dark.svg';
+      return;
+    } else {
+      logoRef.src = '/logo-light.svg';
+      return;
     }
   }
 }
